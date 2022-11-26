3f97738d56f72347e790f6b3642cc5b53e023c91
Lionking789 <dalma20039@gmail.com>
2022-11-26 14:39:32 +0100

    Adatbekérés

---
+++ main.py
@@ -0,0 +1,3 @@
+import rendelés
+
+rendelés.pizza()
---
+++ rendelés.py
@@ -0,0 +1,33 @@
+adat=[]
+uj=[]
+tipus=[]
+méret=[]
+feltét=[]
+ár=[]
+
+
+#def adatbekeres():
+
+
+
+
+
+def pizza():
+    print("Add meg a pizza adatait!")
+    uj= input("Akar új rendelést rögziteni? i/n : ")
+
+    print("*" * 30)
+
+    tipus = input("*1-sajtos *2-gombás *3-sonkás ")
+
+    print("*" * 30)
+
+    méret = input("*1-kicsi *2-normál *3-nagy ")
+
+    print("*" * 30)
+
+    feltét = input("Kér extra feltétet? i/n : ")
+
+
+
+#def arszamitas():
